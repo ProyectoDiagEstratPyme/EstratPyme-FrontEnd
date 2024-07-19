7190c459a7005993fff3cd10b89f15a304829ace
DiegoGiraldoZ <beargiraldo@gmail.com>
2024-07-18 23:44:06 -0500

    Se crea json server y modelo de usuario

---
+++ src/app/models/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: number;
+  mail: string;
+  password: string;
+  nombre: string;
+  sector_empresa: string;
+  tipo_id: number;
+  tipo_empresa: string;
+  tamano: number;
+}
