1a8489852af82c56f6d4709b4e6e135bf8694a22
SantiagoMartinez22 <smyardev2@gmail.com>
2024-08-06 21:35:53 -0500

    se añade componente con la funcionalidad de buscar profesor y agregarlo a un proyecto, ademas de coleccion en el json server

--- src/app/views/dashboard-admin/dashboard-admin.component.ts
+++ src/app/views/dashboard-admin/dashboard-admin.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { AddStudentButtonComponent } from "../../add-student-button/add-student-button.component";
+import { AddStudentButtonComponent } from "../../components/add-student-button/add-student-button.component";
+import { AddTeacherButtonComponent } from "../../components/add-teacher-button/add-teacher-button.component";
 
 @Component({
   selector: 'app-dashboard-admin',
   standalone: true,
-  imports: [AddStudentButtonComponent],
+  imports: [AddStudentButtonComponent, AddTeacherButtonComponent],
   templateUrl: './dashboard-admin.component.html',
   styleUrl: './dashboard-admin.component.scss'
 })
