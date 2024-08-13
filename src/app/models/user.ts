58bccb5b874309bf2df4a0680558f406b8bbb2e7
DiegoGiraldoZ <beargiraldo@gmail.com>
2024-08-12 20:26:00 -0500

    actualización component dashboard-admin

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,9 +1,11 @@
 export interface User {
-    name?: string | null | undefined;
-    email?: string | null | undefined;
-    password?: string | null | undefined;
-    typeUser?: string | null | undefined;
-    id?: string | null | undefined;
-    sizeCompany?: string | undefined;
-    sector?: string | undefined;
+  name?: string | null | undefined;
+  email?: string | null | undefined;
+  password?: string | null | undefined;
+  typeUser?: string | null | undefined;
+  id?: string | null | undefined;
+  sizeCompany?: string | undefined;
+  sector?: string | undefined;
+  registerDate?: string | undefined;
+  isBookDonwloaded?: boolean | undefined;
 }
--- src/app/views/dashboard-admin/dashboard-admin.component.ts
+++ src/app/views/dashboard-admin/dashboard-admin.component.ts
@@ -1,14 +1,75 @@
-import { Component } from '@angular/core';
-import { AddStudentButtonComponent } from "../../components/add-student-button/add-student-button.component";
-import { AddTeacherButtonComponent } from "../../components/add-teacher-button/add-teacher-button.component";
+import { Component, OnInit, signal, inject } from '@angular/core';
+import { Chart, ChartType } from 'chart.js/auto'
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { DashboardService } from '../../services/dashboard.service';
+import { User } from '../../models/user';
+import { groupBy, map, toArray } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-admin',
   standalone: true,
-  imports: [AddStudentButtonComponent, AddTeacherButtonComponent],
   templateUrl: './dashboard-admin.component.html',
-  styleUrl: './dashboard-admin.component.scss'
+  styleUrl: './dashboard-admin.component.scss',
+  imports: [NavbarComponent],
 })
-export class DashboardAdminComponent {
+export class DashboardAdminComponent implements OnInit {
+  users = signal<User[]>([]);
+  year = signal<string>('2024-');
 
+  private dashboardService = inject(DashboardService);
+
+  groupedUsers: any;
+
+  public chart: Chart | undefined;
+
+  ngOnInit(): void {
+    this.getGroupedUsersByMonth();
+    console.log(this.groupedUsers)
+
+    const data = {
+      labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
+      datasets: [
+        {
+          label: 'Total empresas por mes',
+          data: [60, 80, 20, 35, 93, 19, 47],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1,
+        },
+      ],
+    };
+
+    this.chart = new Chart('chart', {
+      type: 'line' as ChartType,
+      data,
+    });
+  }
+
+  getUsers() {
+    this.dashboardService.getUsers().subscribe({
+      next: (users) => {
+        this.users.set(users);
+      },
+    });
+  }
+
+  getGroupedUsersByMonth() {
+    this.dashboardService
+      .getUsers()
+      .pipe(
+        map((users) =>
+          users.filter((user) => user.registerDate?.startsWith(this.year()))
+        ),
+        groupBy(
+          user => user.registerDate?.split('-')[1]
+        ),
+        map((group) => ({
+          month: group.key,
+          users: group.pipe(toArray()).toPromise(),
+        }))
+      )
+      .subscribe(groupedUsers => {
+          this.groupedUsers = groupedUsers;
+      })
+  }
 }
