11d03d840317f0f6bb80816fbc0c1f3648461e61
DiegoGiraldoZ <beargiraldo@gmail.com>
2024-08-12 22:02:55 -0500

    Se crea graficso en dasboard-admin - nuevo campoDB

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -3,10 +3,10 @@ export interface User {
   name?: string | null | undefined;
   email?: string | null | undefined;
   password?: string | null | undefined;
-  typeUser?: string | null | undefined;
-  sizeCompany?: string | undefined;
-  sector?: string | undefined;
-  registerDate: string;
+  typeUser?: string | null | undefined | number;
+  sizeCompany?: string | undefined | number;
+  sector?: string | undefined | number;
+  registerDate?: string;
   isBookDonwloaded?: boolean | undefined;
   isTestDone?: boolean;
 }
--- src/app/views/dashboard-admin/dashboard-admin.component.ts
+++ src/app/views/dashboard-admin/dashboard-admin.component.ts
@@ -3,7 +3,6 @@ import { Chart, ChartType } from 'chart.js/auto'
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { DashboardService } from '../../services/dashboard.service';
 import { User } from '../../models/user';
-import { groupBy, map, toArray } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-admin',
@@ -13,63 +12,178 @@ import { groupBy, map, toArray } from 'rxjs';
   imports: [NavbarComponent],
 })
 export class DashboardAdminComponent implements OnInit {
+  lineChart: Chart | undefined;
+  pieChart: Chart | undefined;
+  radarChart: Chart | undefined;
+  barChart: Chart | undefined;
   users = signal<User[]>([]);
   year = signal<string>('2024-');
+  groupedUsersByMonth: any;
+  groupedUsersBySize: any;
+  groupedUsersBySector: any;
+  groupedUsersByDownload: any;
 
   private dashboardService = inject(DashboardService);
 
-  groupedUsers: any;
-
-  public chart: Chart | undefined;
-
   ngOnInit(): void {
     this.getGroupedUsersByMonth();
-    console.log(this.groupedUsers)
+    this.getGroupedUsersBySize();
+    this.getGroupedUsersBySector();
+    this.getGroupedUsersByDownload();
+  }
 
-    const data = {
-      labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
-      datasets: [
-        {
-          label: 'Total empresas por mes',
-          data: [60, 80, 20, 35, 93, 19, 47],
-          fill: false,
-          borderColor: 'rgb(75, 192, 192)',
-          tension: 0.1,
-        },
-      ],
-    };
+  handleYearChange(event: Event) {
+    this.year.set((event.target as HTMLInputElement).value);
+    this.getGroupedUsersByMonth();
+  }
 
-    this.chart = new Chart('chart', {
-      type: 'line' as ChartType,
-      data,
+  getGroupedUsersByMonth() {
+    this.dashboardService.getUsers().subscribe((users) => {
+      const usersByYear = users.filter((user) =>
+        user.registerDate?.startsWith(this.year())
+      );
+      const userCountByMonth = Array(12).fill(0);
+      usersByYear.forEach((user) => {
+        const userRegisterDate = user.registerDate != null ? user.registerDate : "2024-01"
+        const month = parseInt(userRegisterDate.split('-')[1], 10) - 1;
+        userCountByMonth[month]++;
+      });
+
+      this.groupedUsersByMonth = userCountByMonth;
+      this.updateLineChart();
     });
   }
 
-  getUsers() {
-    this.dashboardService.getUsers().subscribe({
-      next: (users) => {
-        this.users.set(users);
+  getGroupedUsersBySize() {
+    this.dashboardService.getUsers().subscribe((users) => {
+      const small = users.filter((user) => user.sizeCompany == '1').length;
+      const medium = users.filter((user) => user.sizeCompany == '2').length;
+      const large = users.filter((user) => user.sizeCompany == '3').length;
+
+      console.log(small, medium, large);
+
+      this.groupedUsersBySize = [small, medium, large];
+    });
+    this.initializePieChart();
+  }
+
+  getGroupedUsersBySector() {
+    this.dashboardService.getUsers().subscribe((users) => {
+      const primary = users.filter((user) => user.sector == '1').length;
+      const secondary = users.filter((user) => user.sector == '2').length;
+      const tertiary = users.filter((user) => user.sector == '3').length;
+      const quaternary = users.filter((user) => user.sector == '4').length;
+
+      this.groupedUsersBySector = [primary, secondary, tertiary, quaternary];
+    });
+    this.initializeRadarChart();
+  }
+
+  getGroupedUsersByDownload() {
+    this.dashboardService.getUsers().subscribe((users) => {
+      const downloaded = users.filter((user) => user.isBookDonwloaded == true).length;
+      const notDownloaded = users.filter((user) => user.isBookDonwloaded == false).length;
+
+      this.groupedUsersByDownload = [downloaded, notDownloaded];
+    });
+    this.initiliazeBarChart();
+  }
+
+  updateLineChart() {
+    this.lineChart?.destroy();
+
+    this.lineChart = new Chart('lineChart', {
+      type: 'line' as ChartType,
+      data: {
+        labels: [
+          'Enero',
+          'Febrero',
+          'Marzo',
+          'Abril',
+          'Mayo',
+          'Junio',
+          'Julio',
+          'Agosto',
+          'Septiembre',
+          'Octubre',
+          'Noviembre',
+          'Diciembre',
+        ],
+        datasets: [
+          {
+            label: 'Total empresas registradas por mes',
+            data: this.groupedUsersByMonth,
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1,
+          },
+        ],
       },
     });
   }
 
-  getGroupedUsersByMonth() {
-    this.dashboardService
-      .getUsers()
-      .pipe(
-        map((users) =>
-          users.filter((user) => user.registerDate?.startsWith(this.year()))
-        ),
-        groupBy(
-          user => user.registerDate?.split('-')[1]
-        ),
-        map((group) => ({
-          month: group.key,
-          users: group.pipe(toArray()).toPromise(),
-        }))
-      )
-      .subscribe(groupedUsers => {
-          this.groupedUsers = groupedUsers;
-      })
+  initializePieChart() {
+    const ctx = document.getElementById('pieChart') as HTMLCanvasElement;
+    this.pieChart = new Chart(ctx, {
+      type: 'pie' as ChartType,
+      data: {
+        labels: ['Pequeña', 'Mediana', 'Grande'],
+        datasets: [
+          {
+            label: 'Empresas registradas por tamaño',
+            data: this.groupedUsersBySize,
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(255, 205, 86)',
+            ],
+            hoverOffset: 3,
+          },
+        ],
+      },
+    });
+  }
+
+  initializeRadarChart() {
+    const ctx = document.getElementById('radarChart') as HTMLCanvasElement;
+    this.radarChart = new Chart(ctx, {
+      type: 'radar' as ChartType,
+      data: {
+        labels: ['Sector Primario', 'Sector Secundario', 'Sector Terciario', 'Sector Cuaternario'],
+        datasets: [
+          {
+            label: 'Empresas registradas por tamaño',
+            data: this.groupedUsersBySector,
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(255, 205, 86)',
+              'rgb(75, 192, 192)'
+            ],
+            hoverOffset: 4,
+          },
+        ],
+      },
+    });
+  }
+
+  initiliazeBarChart() {
+    const ctx = document.getElementById('barChart') as HTMLCanvasElement;
+    this.barChart = new Chart(ctx, {
+      type: 'bar' as ChartType,
+      data: {
+        labels: ['Lo descargaron', 'No lo descargaron'],
+        datasets: [
+          {
+            label: 'Empresas registradas que descargaron el libro',
+            data: this.groupedUsersByDownload,
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+            ],
+          },
+        ],
+      },
+    });
   }
 }
