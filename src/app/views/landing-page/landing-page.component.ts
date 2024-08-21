617e492e44159bfafda10a0e52e1721312520230
AnthonyCarmine <anthonymunoz302755@correo.itm.edu.co>
2024-08-21 12:28:21 -0500

    Se hace landing page y se ajusta estilo responsive

--- src/app/views/landing-page/landing-page.component.ts
+++ src/app/views/landing-page/landing-page.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterModule, RouterOutlet, NavbarComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent {
 
